ab641497f643c3298ddd6faeb7925ce5b32bf988
Chandra140104 <chandrabgs140104@gmail.com>
2024-09-11 14:48:47 +0700

    Praktikum-2.1

--- database/migrations/2024_09_11_062924_create_m_level_table.php
+++ database/migrations/2024_09_11_062924_create_m_level_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {   
         Schema::create('m_level', function (Blueprint $table) {
-            $table->id();
+            $table->id('level_id');
             $table->timestamps();
             $table->string('level_kode',10)->unique();
             $table->string('level_nama',100);
         });
     }
-    
+
     /**
      * Reverse the migrations.
      */
