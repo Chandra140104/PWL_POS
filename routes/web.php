af00d70f2cf4b879c15ce4a892096d7c6938e801
Chandra140104 <chandrabgs140104@gmail.com>
2024-09-17 14:48:20 +0700

    Praktikum-4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
+// use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route untuk LevelController
+Route::get('/level', [LevelController::class, 'index']);
